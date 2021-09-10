6a66116402607e324925dade44805025ca78ce21
anthony090719 <50929169+anthony090719@users.noreply.github.com>
2021-09-09 20:13:03 -0600

    se agrego la opcion sumar segundo commit

--- Calculadora/src/Calcu.java
+++ Calculadora/src/Calcu.java
@@ -11,9 +11,14 @@ public class Calcu {
 		opcion=Integer.parseInt(JOptionPane.showInputDialog("[1]Sumar"+
 		"/n"+"[2]Restar/n"+"[3]Dividir/n"+"[4]Multiplicar/n"+"Ingresa una opcion/n"));
 		
-		switch(opcion) {
+		switch(opcion) 
+		{
 		
-
+		case 1:
+			num1=Double.parseDouble(JOptionPane.showInputDialog("escriba el primer valor: "));
+			num2=Double.parseDouble(JOptionPane.showInputDialog("escriba el segundo valor: "));
+			res=num1+num2;
+			JOptionPane.showMessageDialog(null, "El resultado es: "+res);
 	}
 		
 		
