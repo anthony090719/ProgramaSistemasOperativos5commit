ee3072b052b31aef7073414bd6791e1317026cc7
anthony090719 <50929169+anthony090719@users.noreply.github.com>
2021-09-09 19:44:48 -0600

    Primer commit añadir variables

---
+++ Calculadora/src/Calcu.java
@@ -0,0 +1,22 @@
+import javax.swing.JOptionPane;
+
+
+public class Calcu {
+
+	public static void main(String[] args) {
+		
+		int opcion=0;
+		double num1, num2, res;
+		
+		opcion=Integer.parseInt(JOptionPane.showInputDialog("[1]Sumar"+
+		"/n"+"[2]Restar/n"+"[3]Dividir/n"+"[4]Multiplicar/n"+"Ingresa una opcion/n"));
+		
+		switch(opcion) {
+		
+
+	}
+		
+		
+
+	}
+}
